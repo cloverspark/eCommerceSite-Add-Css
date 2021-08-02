be31bd2b13189d7351ee9277e3fa0b11fed1444f
cloverspark <alexbrenna1999@hotmail.com>
2021-08-02 14:59:27 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,10 @@
+![.NET Core CI](https://github.com/JoeProgrammer88/CPW213-CommerceSite/workflows/.NET%20Core%20CI/badge.svg)
+
+# CPW213-CommerceSite
+Basic eCommerce site to learn ASP.NET Core MVC Concepts. 
+This is an in-class project for the CPW 213 course.
+
+## Prerequisites
+- [.NET Core 3.1.X SDK](https://dotnet.microsoft.com/download)
+- [Visual Studio](https://visualstudio.microsoft.com/) with ASP.NET and Web Dev workload OR VS Code
+
---
+++ eCommerceSite/Controllers/ProductController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductContext _context;
+
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Displays a view that lists all products
+        /// </summary>
+        public async Task<IActionResult> Index()
+        {
+            // Get all products from database
+            // List<Product> products = _context.Products.ToList();
+            List<Product> products =
+                await (from p in _context.Products
+                        select p).ToListAsync();
+            
+
+            // Send list of products to view to be displayed
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add to DB
+                _context.Products.Add(p);
+                await _context.SaveChangesAsync();
+
+                TempData["Message"] = $"{p.Title} was added successfully";
+
+                // redirect back to catalog page
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            // Get product with corresponding id
+            Product p =
+                await(from prod in _context.Products
+                      where prod.ProductId == id
+                      select prod).SingleAsync();
+
+            //Product p2 = await _context
+            //                .Products
+            //                .Where(prod => prod.ProductId == id)
+            //                .SingleAsync();
+
+            // pass product to view
+            return View(p);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(p).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+
+                ViewData["Message"] = "Product updated successfully";
+            }
+
+            return View(p);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Product p = await (from prod in _context.Products
+                        where prod.ProductId == id
+                        select prod).SingleAsync();
+
+            return View(p);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            Product p = await (from prod in _context.Products
+                        where prod.ProductId == id
+                        select prod).SingleAsync();
+
+            _context.Entry(p).State = EntityState.Deleted;
+
+            await _context.SaveChangesAsync();
+
+            TempData["Message"] = $"{p.Title} was deleted";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
